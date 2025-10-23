707441457ceb02d187680a675b2f273e602a99fc
leo <leoanrdomule@gmail.com>
2025-10-23 13:43:25 -0300

    hecho con IA

--- Escribe_el_código.py/index.py
+++ Escribe_el_código.py/index.py
@@ -10,7 +10,7 @@ ventana.geometry("1366x768")
 titulo = tk.Label(text="¡Escribe el código!", font=("Arial", 25, "bold"))
 pregunta = tk.Label(text="¿Cómo mostrarías tu nombre en pantalla?", font=("Arial", 15, "bold"))
 respuesta = tk.Entry(ventana, bd=1, relief="solid", font=("Cascadia Code", 15))
-comprobar = tk.Button(text="¡Comprobar!", font=("Cascadia Code", 15, "bold"))
+comprobar = tk.Button(text="¡Comprobar!", relief="groove", font=("Cascadia Code", 15, "bold"))
 
 # Mostramos los objetos
 titulo.pack()
