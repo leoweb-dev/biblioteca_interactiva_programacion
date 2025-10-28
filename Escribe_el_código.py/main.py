8a39e2daf275fbbc13c7d005ab9d5c55e5c60887
leo <leoanrdomule@gmail.com>
2025-10-28 11:09:46 -0300

    Cambios en la configuración

--- Escribe_el_código.py/main.py
+++ Escribe_el_código.py/main.py
@@ -8,7 +8,7 @@ ventana.geometry("1366x768")
 
 # Creamos los objetos
 titulo = tk.Label()
-pregunta = tk.Label(text="¿Cómo mostrarías tu nombre en pantalla?", font=("Arial", 15, "bold"))
+pregunta = tk.Label()
 respuesta = tk.Text(ventana)
 comprobar = tk.Button(text="¡Comprobar!", relief="groove", font=("Cascadia Code", 15, "bold"))
 
@@ -16,6 +16,7 @@ comprobar = tk.Button(text="¡Comprobar!", relief="groove", font=("Cascadia Code
 respuesta.config(width=80, height=15, font=("Cascadia Code", 15), background="#080808", foreground="white", insertbackground="white")
 ventana.config(background='#242424')
 titulo.config(text="¡Escribe el código!", font=("Arial", 25, "bold"),background='#242424', foreground="white")
+pregunta.config(text="¿Cómo mostrarías tu nombre en pantalla?", font=("Arial", 15, "bold"),background='#242424', foreground="white")
 
 # Mostramos los objetos
 titulo.pack()
